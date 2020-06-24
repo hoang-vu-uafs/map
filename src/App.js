fd0b8b234351986b0abb57e89520927a7ee0cbd4
Hoang Vu <hoangvu@uafs.edu>
2020-06-23 19:24:07 -0500

    googl map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    ready: false,
+  };
+
+  componentDidMount() {
+    this.setState({
+      ready: true,
+    });
+  }
+
+  componentDidUpdate() {
+    this.initMap();
+  }
+
+  initMap() {
+    this.map = new window.google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: -34.397,
+        lng: 150.644,
+      },
+      zoom: 8,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div style={{ width: 400, height: 300 }} id="map"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
